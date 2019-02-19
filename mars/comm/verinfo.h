8a39fa1907ccd7d9f7c5f9e882c783c1765efc2a
xiaozhi <20785672@qq.com>
2019-02-19 16:41:20 +0800

    revert comiler

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "ab7917b"
+#define MARS_REVISION "49bb4c9"
 #define MARS_PATH "190118"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-01-18 21:46:50"
+#define MARS_BUILD_TIME "2019-01-19 18:22:58"
 #define MARS_TAG ""
 
 #endif
--- mars/zmake2017.bat
+++ mars/zmake2017.bat
@@ -1,2 +1,5 @@
+@rem mkdir proj.ios
+@rem cd proj.ios
+@rem cmake -G "Xcode" ..
 cmake -G "Visual Studio 15 Win64"
 pause
