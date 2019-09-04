58001a79f5fe4b55b2b4dc8a10558c22e0812370
xiaozhi <20785672@qq.com>
2019-09-04 15:37:57 +0800

    add: raknetSample

--- mars/comm/verinfo.h
+++ mars/comm/verinfo.h
@@ -2,10 +2,10 @@
 #ifndef Mars_verinfo_h
 #define Mars_verinfo_h
 
-#define MARS_REVISION "f630001c"
+#define MARS_REVISION "0130fb3a"
 #define MARS_PATH "190118_Rak"
 #define MARS_URL ""
-#define MARS_BUILD_TIME "2019-09-04 11:10:56"
+#define MARS_BUILD_TIME "2019-09-04 11:17:11"
 #define MARS_TAG ""
 
 #endif
