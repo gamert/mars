09ab0e82b3362f62976ea4608232ace508b2eaf0
xiaozhi <20785672@qq.com>
2019-09-04 22:38:43 +0800

    ok for android ndk

--- samples/android/raknetSample/app/src/main/jni/native-lib.cpp
+++ samples/android/raknetSample/app/src/main/jni/native-lib.cpp
@@ -5,6 +5,20 @@
 #include "xlogger/xlogger.h"
 
 #include "xlogger/android_xlog.h"
+#include "G:\xProject_dp\SDK\mars\mars\comm/socket/unix_socket.h"
+
+//#include <jni.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <android/log.h>
+//#include <sys/socket.h>
+//#include <sys/types.h>
+//#include <sys/un.h>
+//#include <netinet/in.h>
+//#include <stddef.h>
+//#include <arpa/inet.h>
+//#include <unistd.h>
+//#include <in.h>
 
 namespace RakNet
 {
@@ -19,6 +33,43 @@ struct _Rak_Settings_t
 #define T_DLL
 #define STDCALL
 #include "G:\xProject_dp\SDK\mars\mars\raknet\src\RakNetConnectorDef.h"
+
+
+int udp_socket()
+{
+    int udpSocket = socket(PF_INET, SOCK_DGRAM, 0);
+
+    if (udpSocket == -1) {
+        return -1;
+    }
+
+    __android_log_print(ANDROID_LOG_ERROR, "socket", "udpSocket\n");
+
+    sockaddr_in address;
+
+    memset(&address, 0, sizeof(address));
+    address.sin_family = PF_INET;
+
+    address.sin_addr.s_addr = htonl(INADDR_ANY);
+    address.sin_port = htons(12346);
+
+    int bindState = bind(udpSocket, (const struct sockaddr*)&address, sizeof(address));
+
+    if (bindState == -1) {
+        return -1;
+    }
+
+    char cc[100];
+
+    struct sockaddr_in recvfromAddress;
+    memset(&recvfromAddress, 0, sizeof(recvfromAddress));
+    socklen_t l = sizeof(struct sockaddr_in);
+
+    ssize_t recvSize = recvfrom(udpSocket, cc, 8, 0, (struct sockaddr*)&recvfromAddress, &l);
+
+    __android_log_print(ANDROID_LOG_ERROR, "socket", "recvSize\n");
+    return 0;
+}
 
 int id = -1;
 extern "C"
@@ -56,7 +107,9 @@ Java_com_tencent_mars_xlogsample_MainActivity_stringFromJNI(
 	
 //	LOGI("test", "0000");
 
-
+//    __android_log_print(ANDROID_LOG_INFO, "test", "udp_socket:%d", s);
+//    int s = udp_socket();
+//    __android_log_print(ANDROID_LOG_INFO, "test", "udp_socket:%d", s);
 
 	_Rak_Settings_t ss = {0,0};
 	RkN_Initialize2(&ss,"","");
@@ -66,6 +119,15 @@ Java_com_tencent_mars_xlogsample_MainActivity_stringFromJNI(
         id = RkN_CreatePeer();
         __android_log_print(ANDROID_LOG_INFO, "test", "RkN_CreatePeer:%d", id);
         //	LOGI("test", "2222");
+
+//        int ret;
+//        unsigned short addressFamily = AF_INET;
+//        int type = SOCK_STREAM;
+//        int protocol = 0;
+//        int rns2Socket = (int) ::socket( addressFamily, type, protocol );
+//        __android_log_print(ANDROID_LOG_INFO, "test", "socket:%d", rns2Socket);
+
+
         int res = RkN_Startup(id,"0.0.0.0", 0, 1);
         __android_log_print(ANDROID_LOG_INFO, "test", "RkN_Startup,id=%d, res=%d", id,res);
     }
